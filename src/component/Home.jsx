682781ddd0673e96765982eceb4528b4820017b4
“Salu <alhassansalu1@gmil.com>
2024-02-09 21:01:33 +0000

    hero update

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,17 +1,26 @@
 
 
 export default function Home(){
-
+   
     return(
         <div className="bg-pro-200 ">
            {/*  Hero section */}
-            <div className="bg-gradient-to-l from-pro-300 h-120 flex flex-row" >
-                <div className="w-1/2">
-                    <img className="h-120 w-120 pt-2" src="./src/assets/globe.svg" alt="svg of globe" />
+            <div className="bg-gradient-to-l from-pro-300 flex flex-col md:flex-row gap-2" >
+                <div className="flex flex-col justify-center p-10">
+                    <h1 className="text-xl font-semibold">
+                        We are Creative and Innovative developers
+                    </h1>      
+                    <p>Tackling a wide range of global issues is our main core focus. Education,Bussiness, Human Rights, Poverty Alleviation, Global Connectivity, and health fall in our scope.
+                    </p> 
+                    <button
+                    className="">
+                        learn more
+                    </button>
                 </div>
-                <div className="w-1/2">
-                    
+                <div className="">
+                    <img className="h-96 md:h-120 md:w-120 md:pt-2" src="./src/assets/globe.svg" alt="svg of globe" />
                 </div>
+               
             </div>
         </div>
     );
