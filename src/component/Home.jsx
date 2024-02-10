bd75f2ffa7011fb3866aabd0e41106b99212bca3
“Salu <alhassansalu1@gmil.com>
2024-02-10 19:27:51 +0000

    Our services

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -11,7 +11,6 @@ export default function Home(){
         <div className="bg-pro-200 ">
            {/*  Hero section */}
             <div 
-            
             className="bg-gradient-to-l from-pro-300 flex flex-col md:flex-row gap-1" >
                 <div className="flex flex-col justify-center p-12 md:pl-28 ">
                     <h1 
@@ -24,16 +23,23 @@ export default function Home(){
                     </h1>      
                     <button
                     onClick={handleMoreClick}
-                    className="text-white bg-pro-100 h-10 rounded-lg mt-10 md:w-120 ">
+                    className="text-white bg-pro-100 h-10 rounded-lg mt-10 md:w-120 text-semibold">
                         {showMore ? 'Hide' : 'Show'} details
                     </button>
                     {showMore && 
                     <p 
-                    className='bg-pro-300 mt-0 p-4 text-white rounded-lg md:w-120 transition duration-700 ease-in-out'>
+                    className='bg-pro-300 mt-0 p-4 text-white rounded-lg md:w-120 '>
                         Tackling a wide range of global issues is our main core focus. Education,Bussiness, Human Rights, Poverty Alleviation, Global Connectivity, and health fall in our scope.</p>}
                 </div>
-            
                 <img className="hidden md:block md:pl-64 md:h-120 md:pt-2" src="./src/assets/globe.svg" alt="svg of globe" />
+            </div>
+            <div className='bg-black '>
+                <div className="">
+                    <h1 className='text-pro-100 font-semibold '>Our Services</h1>
+                </div>
+                <div className="">
+
+                </div>
 
             </div>
         </div>
