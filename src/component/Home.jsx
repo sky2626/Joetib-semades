220f81ea1bde7da5f073c9b37356e59898738617
“Salu <alhassansalu1@gmil.com>
2024-02-10 20:18:29 +0000

    update on our service

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -2,9 +2,13 @@ import { useState } from 'react';
 
 export default function Home(){
     const [showMore, setShowMore] = useState(false);
+    const [showMore2, setShowMore2] = useState(true);
     
     function handleMoreClick() {
         setShowMore(!showMore);
+      }
+    function handleMoreClick2() {
+        setShowMore2(!showMore2);
       }
     
     return(
@@ -34,8 +38,26 @@ export default function Home(){
                 <img className="hidden md:block md:pl-64 md:h-120 md:pt-2" src="./src/assets/globe.svg" alt="svg of globe" />
             </div>
             <div className='bg-black '>
-                <div className="">
-                    <h1 className='text-pro-100 font-semibold '>Our Services</h1>
+                <div className="flex justify-center flex-col p-12">
+                    <h2 className='text-pro-100 font-semibold text-center md:text-start text-2xl'>
+                        Our Services
+                    </h2>
+                    <h1 className='text-white font-semibold text-center md:text-start text-5xl '>
+                        What we <span className='text-pro-100'>do</span>
+                    </h1>
+                    <div className='bg-pro-100 rounded-sm h-2 w-20 ml-12 mt-1'></div>
+                    <button
+                    onClick={handleMoreClick2}
+                    className="text-white bg-pro-100 h-10 w-24 rounded-lg mt-10 md:w-120 text-semibold">
+                        {showMore2 ? 'Hide' : 'Show'} details
+                    </button>
+                    {showMore2 &&
+                    <p className='bg-pro-300 mt-0 p-4 text-white rounded-lg md:w-120 '>
+                        web development encompasses the creation of websites and web applications, involving front-end, back-end, databases, and hosting.
+                        Cloud infrastructure and services to create, deploy, and manage applications with advantages such as scalability, cost efficiency, 
+                        and flexibility. Machine learning is a subset of artificial intelligence (AI) that focuses on creating algorithms and models that 
+                        enable computers to learn from data and make predictions or decisions without explicit programming.
+                    </p>}
                 </div>
                 <div className="">
 
