d3cc6b5077aadb88f9c830786958edb2e89af3d6
“Salu <alhassansalu1@gmil.com>
2024-02-17 23:45:04 +0000

    update on contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,24 +1,35 @@
 import Footer from '../component/Footer'
+import { FcPhone } from "react-icons/fc"
 
 
 export default function Contact () {
     return(
         <div >
             <div 
-            className="bg-gradient-to-r from-pro-300 to-pro-200 h-96 flex justify-center items-center">
+            className="bg-gradient-to-r from-pro-300 to-pro-200 h-96 flex flex-col justify-center items-center">
                 <h1 
-                className="font-Cascadia font-extrabold text-5xl md:text-6xl text-center text-white px-4">Let's have a talk</h1>
+                className="font-Cascadia font-extrabold text-5xl md:text-6xl text-center text-white px-4">
+                    Let's have a talk
+                </h1>
+                <p 
+                className="p-4 text-slate-400 text-center md:px-24">
+                We're just one click away to help you take your brand or product from great to incredible. 
+                Fill in the form to share more details about your project or your problems. 
+                Either way, we’d love to talk.
+                </p>
             </div> 
             <div 
             className="bg-pro-200">
-                <p 
-                className="p-4 text-white text-2xl md:px-24">
-                We're just one click away to help you take your brand or product from great to incredible. 
-                Fill in the form to share more details about your project. Or your favorite gum flavor. 
-                Either way, we’d love to talk.
-                </p>
+                
                 <div 
                 className='bg-pro-200 flex px-24 py-16 justify-center md:gap-16 lg:gap- md:px-24 '>
+                    <div>
+                        <h1>Contact Information</h1>
+                        <div className='flex flex-row justify-center items-center'>
+                            <img className='h-16 p-4' src="../src/assets/mail.svg" alt="mail img" />
+                            <a className='text-xl text-slate-500' href="mailto:webmaster@example.com">semades@mail.gh</a>
+                        </div>
+                    </div>
                     <div 
                     class="flex flex-col gap-2 md:justify-start md:items-start ">
                         <input
@@ -35,7 +46,6 @@ export default function Contact () {
                             Submit
                         </button>
                     </div>
-                    <img className='hidden md:block h-80 ' src="./src/assets/support.svg " alt="img of support" />
                 </div> 
             </div>
 
