f4cea8e1e94053e90276209850f720561535947e
“Salu <alhassansalu1@gmil.com>
2024-02-22 20:54:57 +0000

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,9 @@
 export default function About() {
     return(
         <div>
-            <h1 className="font-Cascadia font-extrabold text-3xl">About section here</h1>
+            <div>
+                <h1 className="font-Cascadia font-extrabold text-3xl">About section here</h1>
+            </div>  
         </div>
     );
 }
